b986c36db724468860500390941ab34a54496f07
NJUYSX <1040646386@qq.com>
2022-11-15 23:54:27 +0800

    commit_Judgement

---
+++ Judgement.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Input.h"
+#include "Output.h"
+#include <stdio.h> 
+using namespace std;
+class Judgement {
+private:
+	Input* input1;
+	Input* input2;
+	Output* output;
+	vector<Stdin_format*> Format_vector;
+public:
+	Judgement();
+	Judgement(Input* input_1, Input* input_2, Output* _output, vector<Stdin_format*> Vector);
+	void GenerateRandomCase();//generate random cases into casefile for test
+	void Judge();//run the test to judge if the two program are equal
+
+};
+string random_case(int n);//generate single random test
+
+void runJudgement();//combine functions above and run
